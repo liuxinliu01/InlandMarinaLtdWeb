d1d66fdf65d2fbce9af1824df3a751e56d002616
欣 刘 <liuxinliu_01@hotmail.com>
2019-10-19 13:50:22 -0600

    upload the final version

---
+++ Desktop/Workshop4FInal-final/TravelExperts/TravelExperts/PackagesForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TravelExperts
+{
+    partial class PackagesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fullpackageGridView = new System.Windows.Forms.DataGridView();
+            this.fullPackageListView = new System.Windows.Forms.ListView();
+            this.PackageName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PackageDesc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PackageStartDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PackageSupplierName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PackageProductName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.packageDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.packageGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.fullpackageGridView)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.packageGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fullpackageGridView
+            // 
+            this.fullpackageGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fullpackageGridView.Location = new System.Drawing.Point(27, 345);
+            this.fullpackageGridView.Name = "fullpackageGridView";
+            this.fullpackageGridView.RowHeadersWidth = 62;
+            this.fullpackageGridView.RowTemplate.Height = 28;
+            this.fullpackageGridView.Size = new System.Drawing.Size(1151, 137);
+            this.fullpackageGridView.TabIndex = 0;
+            // 
+            // fullPackageListView
+            // 
+            this.fullPackageListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PackageName,
+            this.PackageDesc,
+            this.PackageStartDate,
+            this.PackageSupplierName,
+            this.PackageProductName});
+            this.fullPackageListView.Location = new System.Drawing.Point(27, 500);
+            this.fullPackageListView.Name = "fullPackageListView";
+            this.fullPackageListView.Size = new System.Drawing.Size(1141, 254);
+            this.fullPackageListView.TabIndex = 1;
+            this.fullPackageListView.UseCompatibleStateImageBehavior = false;
+            this.fullPackageListView.View = System.Windows.Forms.View.Details;
+            // 
+            // PackageName
+            // 
+            this.PackageName.Text = "Package Name";
+            this.PackageName.Width = 200;
+            // 
+            // PackageDesc
+            // 
+            this.PackageDesc.Text = "Package Description";
+            this.PackageDesc.Width = 250;
+            // 
+            // PackageStartDate
+            // 
+            this.PackageStartDate.Text = "PackageStartDate";
+            this.PackageStartDate.Width = 200;
+            // 
+            // PackageSupplierName
+            // 
+            this.PackageSupplierName.Text = "Supplier";
+            this.PackageSupplierName.Width = 200;
+            // 
+            // PackageProductName
+            // 
+            this.PackageProductName.Text = "ProductName";
+            this.PackageProductName.Width = 200;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.packageDetailsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 69);
+            // 
+            // packageDetailsToolStripMenuItem
+            // 
+            this.packageDetailsToolStripMenuItem.Name = "packageDetailsToolStripMenuItem";
+            this.packageDetailsToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.packageDetailsToolStripMenuItem.Text = "Package Details....";
+            this.packageDetailsToolStripMenuItem.Click += new System.EventHandler(this.PackageDetailsToolStripMenuItem_Click);
+            // 
+            // packageGridView
+            // 
+            this.packageGridView.AllowUserToOrderColumns = true;
+            this.packageGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.packageGridView.ContextMenuStrip = this.contextMenuStrip1;
+            this.packageGridView.Location = new System.Drawing.Point(79, 34);
+            this.packageGridView.Name = "packageGridView";
+            this.packageGridView.RowHeadersWidth = 62;
+            this.packageGridView.RowTemplate.Height = 28;
+            this.packageGridView.Size = new System.Drawing.Size(935, 189);
+            this.packageGridView.TabIndex = 4;
+            // 
+            // PackagesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1228, 679);
+            this.Controls.Add(this.packageGridView);
+            this.Controls.Add(this.fullPackageListView);
+            this.Controls.Add(this.fullpackageGridView);
+            this.Name = "PackagesForm";
+            this.Text = "PackageForm";
+            this.Load += new System.EventHandler(this.PackagesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fullpackageGridView)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.packageGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView fullpackageGridView;
+        private System.Windows.Forms.ListView fullPackageListView;
+        private System.Windows.Forms.ColumnHeader PackageName;
+        private System.Windows.Forms.ColumnHeader PackageDesc;
+        private System.Windows.Forms.ColumnHeader PackageStartDate;
+        private System.Windows.Forms.ColumnHeader PackageSupplierName;
+        private System.Windows.Forms.ColumnHeader PackageProductName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem packageDetailsToolStripMenuItem;
+        private System.Windows.Forms.DataGridView packageGridView;
+    }
+}
+
---
+++ Desktop/Workshop4FInal-final/TravelExperts/TravelExperts/PackagesForm.cs
@@ -0,0 +1,105 @@
+﻿using ProductSupplier;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelExperts
+{
+    public partial class PackagesForm : Form
+    {
+        fullPackageDataContext dbContext = new fullPackageDataContext();
+        List<Package> PackageList = new List<Package>();
+        List<Supplier> SupplierList = new List<Supplier>();
+        List<Product> ProductList = new List<Product>();
+        List<Products_Supplier> Products_SupplierList = new List<Products_Supplier>();
+        List<Packages_Products_Supplier> Packages_Products_SupplierList = new List<Packages_Products_Supplier>();
+
+        public PackagesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PackagesForm_Load(object sender, EventArgs e)
+        {
+
+
+            using (fullPackageDataContext dbContext = new fullPackageDataContext())
+            {
+                packageGridView.DataSource = dbContext.Packages;
+                foreach (Package p in dbContext.Packages)
+                {
+                    PackageList.Add(p);
+
+                }
+
+                var packages = from pps in dbContext.Packages_Products_Suppliers
+                               join pk in dbContext.Packages
+                               on pps.PackageId equals pk.PackageId
+                               join ps in dbContext.Products_Suppliers
+                               on pps.ProductSupplierId equals ps.ProductSupplierId
+                               join s in dbContext.Suppliers
+                               on ps.SupplierId equals s.SupplierId
+                               join p in dbContext.Products
+                               on ps.ProductId equals p.ProductId
+                               select new
+                               {
+                                   pk.PackageId,
+                                   pk.PkgName,
+                                   pk.PkgDesc,
+                                   pk.PkgStartDate,
+                                   //pk.PkgEndDate,
+                                   //pk.PkgBasePrice,
+                                   //pk.PkgAgencyCommission,
+                                   s.SupName,
+                                   p.ProdName
+
+                               };
+                string packageName = "";
+                int i = 0;
+                foreach (var package in packages)
+                {
+                    if (package.PkgName != packageName)
+                    {
+                        fullPackageListView.Items.Add(package.PkgName);
+                        packageName = package.PkgName;
+                    }
+                    else
+                    {
+                        fullPackageListView.Items.Add("");
+                    }
+                    fullPackageListView.Items[i].SubItems.Add(package.PkgDesc.ToString());
+                    fullPackageListView.Items[i].SubItems.Add(
+                       Convert.ToDateTime(package.PkgStartDate).ToShortDateString());
+                    fullPackageListView.Items[i].SubItems.Add(package.SupName.ToString());
+                    fullPackageListView.Items[i].SubItems.Add(package.ProdName.ToString());
+                    //fullPackageListView.Items[i].SubItems.Add(package.PkgDesc);
+                    //fullPackageListView.Items[i].SubItems.Add(package.PkgDesc);
+                    i += 1;
+
+                }
+                fullpackageGridView.DataSource = packages;
+            }
+
+
+        }
+
+        private void PackageDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int rowNum = packageGridView.CurrentCell.RowIndex;
+            AddPackageForm apf = new AddPackageForm();
+            apf.ReadOnly = true;
+            apf.currentPackage = PackageList[rowNum];
+            DialogResult result = apf.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+        }
+    }
+}
